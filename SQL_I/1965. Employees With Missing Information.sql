3abb6c504ed859ed3668a4c72abe429f09f8cd11
Reza <rezabaiust@gmail.com>
2022-06-07 23:08:02 +0600

    sql_I day_4

---
+++ SQL_I/1965. Employees With Missing Information.sql
@@ -0,0 +1,81 @@
+Employees
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the name of the employee whose ID is employee_id.
+ 
+
+Table: Salaries
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the salary of the employee whose ID is employee_id.
+ 
+
+Write an SQL query to report the IDs of all the employees with missing information. The information of an employee is missing if:
+
+The employee's name is missing, or
+The employee's salary is missing.
+Return the result table ordered by employee_id in ascending order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employees table:
++-------------+----------+
+| employee_id | name     |
++-------------+----------+
+| 2           | Crew     |
+| 4           | Haven    |
+| 5           | Kristian |
++-------------+----------+
+Salaries table:
++-------------+--------+
+| employee_id | salary |
++-------------+--------+
+| 5           | 76071  |
+| 1           | 22517  |
+| 4           | 63539  |
++-------------+--------+
+Output: 
++-------------+
+| employee_id |
++-------------+
+| 1           |
+| 2           |
++-------------+
+Explanation: 
+Employees 1, 2, 4, and 5 are working at this company.
+The name of employee 1 is missing.
+The salary of employee 2 is missing.
+
+
+-------------------Solution
+
+SELECT Employees.employee_id 
+    FROM Employees
+    LEFT JOIN Salaries ON Salaries.employee_id = Employees.employee_id
+    WHERE Salaries.salary IS NULL
+    
+    UNION
+    
+SELECT Salaries.employee_id 
+    FROM Salaries
+    LEFT JOIN Employees ON Employees.employee_id = Salaries.employee_id
+    WHERE Employees.name IS NULL  
+    
+    ORDER BY employee_id;
